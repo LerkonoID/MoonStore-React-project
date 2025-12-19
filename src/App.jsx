4d7abcb48b673beb7509345240a97a20166927d4
LerkonoID <b.korz2005@gmail.com>
2025-12-19 17:46:25 +0200

    Add SnowFall effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Snowfall from 'react-snowfall';
+
 import Home from './pages/Home';
 import About from './pages/About';
 import Contacts from './pages/Contacts.jsx';
@@ -11,10 +13,12 @@ import Cart from './pages/Cart';
 import Footer from './components/Footer.jsx';
 import Header from './components/Header.jsx';
 import NotFound from './pages/NotFound.jsx';
+
 import './styles/App.css';
 
 function App() {
   const [loading, setLoading] = useState(true);
+  const snowColor = '#ffffff';
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -28,24 +32,37 @@ function App() {
   }
 
   return (
-    <Router>
-      <div className="app-container">
-        <Header />
-        <main className="main-content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/product/:id" element={<ProductDetail />} />
-            <Route path="/contacts" element={<Contacts />} />
-            <Route path="/delivery" element={<Delivery />} />
-            <Route path="/success" element={<Success />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+      <>
+        <Snowfall
+            color={snowColor}
+            snowflakeCount={150}
+            style={{
+              position: 'fixed',
+              width: '100vw',
+              height: '100vh',
+              zIndex: 1000,
+            }}
+        />
+
+        <Router>
+          <div className="app-container">
+            <Header />
+            <main className="main-content">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/product/:id" element={<ProductDetail />} />
+                <Route path="/contacts" element={<Contacts />} />
+                <Route path="/delivery" element={<Delivery />} />
+                <Route path="/success" element={<Success />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </Router>
+      </>
   );
 }
 
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Snowfall} from "react-snowfall";
 
 const About = () => {
   return (
